dc67b000b4e0de3a9bf01ed3756120e12d78dfa5
Connor MacKenzie <connor@layerci.com>
2021-11-15 18:05:48 -0500

    Add more cypress tests

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -5,4 +5,25 @@ describe('Visit Home Page', () => {
 
     expect(true).to.equal(false)
   })
+
+  it('This tests passes', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+})
+
+describe('Another one', () => {
+  it('Loads', () => {
+    cy.visit('/')
+    cy.contains('Hello World')
+
+    expect(true).to.equal(false)
+  })
+
+  it('This tests passes', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
 })
