b1ed3fc1a1f40b87455a936b8df3b7f7028a0da4
Connor MacKenzie <connor@layerci.com>
2021-11-24 20:29:40 -0500

    Change cypress tests

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,19 @@
+describe('Visit Home Page', () => {
+  it('Loads', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+
+  it('Displays content', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+
+  it('Has working links', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(false)
+  })
+})
---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,25 @@
+describe('Logs into dashbaord', () => {
+  it('Loads', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+
+  it('Renders log in', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+
+  it('Can log in', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+
+  it('Redirects to dashbaord', () => {
+    cy.visit('/')
+
+    expect(true).to.equal(true)
+  })
+})
